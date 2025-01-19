a8be9355a9676952105e76db3d298d5dfb269016
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2025-01-19 15:29:14 +0530

    Update Partition List.java

--- Linked List/Q7. Partition List/Partition List.java
+++ Linked List/Q7. Partition List/Partition List.java
@@ -1,9 +1,9 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode result = new ListNode(0);
-        ListNode dummy = result;
-        ListNode secondList = new ListNode(0);
-        ListNode secondHead = secondList;
+        ListNode result = new ListNode(0); //result node to build the first list
+        ListNode dummy = result; //dummy node to remember the head of the first list
+        ListNode secondList = new ListNode(0); //head node for the second list
+        ListNode secondHead = secondList; //dummy node to remember the head of the second list.
         while(head!=null){
             if(head.val<x){
                 result.next = head;
@@ -15,7 +15,9 @@ class Solution {
             }
             head = head.next;
         }
+        secondList.next = null;
         result.next = secondHead.next;
         return dummy.next;
     }
 }
+
