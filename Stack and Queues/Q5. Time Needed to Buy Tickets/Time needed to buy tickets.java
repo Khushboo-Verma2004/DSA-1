0e918818aaee97f52a49cd0c56504a65e2d30167
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2025-01-19 12:51:55 +0530

    Create Time needed to buy tickets.java

---
+++ Stack and Queues/Q5. Time Needed to Buy Tickets/Time needed to buy tickets.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int n = tickets.length;
+        int count = 0;
+        while(true){
+            
+        }
+        
+    }
+}
