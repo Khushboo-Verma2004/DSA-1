2d919975d55c540df6a6755709cf91acedeea6e7
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2025-01-21 14:06:09 +0530

    Update Sort characters by frequency.java

--- Strings/Q4. Sort Characters By Frequency/Sort characters by frequency.java
+++ Strings/Q4. Sort Characters By Frequency/Sort characters by frequency.java
@@ -1,1 +1,28 @@
 
+class Solution {
+    public String frequencySort(String s) {
+        int[] freq = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+        StringBuilder result = new StringBuilder();
+        while (true) {
+            int maxFreq = 0;
+            char maxChar = 0;
+            for (int i = 0; i < 128; i++) {
+                if (freq[i] > maxFreq) {
+                    maxFreq = freq[i];
+                    maxChar = (char) i;
+                }
+            }
+            if (maxFreq == 0) {
+                break;
+            }
+            while (maxFreq-- > 0) {
+                result.append(maxChar);
+            }
+            freq[maxChar] = 0;
+        }
+        return result.toString();
+    }
+}
