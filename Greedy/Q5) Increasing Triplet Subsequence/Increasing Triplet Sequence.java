112207a722538daf0f3d393a655c980c96ff9662
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2025-01-23 00:36:17 +0530

    Update Increasing Triplet Sequence.java

--- Greedy/Q5) Increasing Triplet Subsequence/Increasing Triplet Sequence.java
+++ Greedy/Q5) Increasing Triplet Subsequence/Increasing Triplet Sequence.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length<=2){
+            return false;
+        }
+        int one = Integer.MAX_VALUE;
+        int two = Integer.MAX_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]<=one){
+                one = nums[i];
+            }
+            else if(nums[i]<=two){
+                two = nums[i];
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
