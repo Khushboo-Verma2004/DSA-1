601518bb9aa917e185a41c40dc58b49fffe5907a
Riya Chaurasia <155444751+Chaurasiariya227@users.noreply.github.com>
2025-01-27 22:21:35 +0530

    Update Chaurasiariya--PowerofTwo.py

--- Recursion/Q1.Power Of Two/Chaurasiariya--PowerofTwo.py
+++ Recursion/Q1.Power Of Two/Chaurasiariya--PowerofTwo.py
@@ -1,19 +1,20 @@
 def isPowerOfTwo(n: int) -> bool:
-  
-    # A number less than or equal to zero cannot be a power of two
-    if n <= 0:
+   
+    if n == 1:  #base case
+        return True
+
+    # If n is less than 1 or not divisible by 2, it cannot be a power of two
+    if n <= 0 or n % 2 != 0:
         return False
-      
-    # n & (n - 1) will be 0 if n is a power of two
-    return (n & (n - 1)) == 0
 
-def main():
-    # Examples for testing
+    #recursion application
+    return isPowerOfTwo(n // 2)
+
+def main():   # main function
     test_cases = [1, 16, 3]
-
     for n in test_cases:
         result = isPowerOfTwo(n)
         print(f"Is {n} a power of two? {result}")
-
 if __name__ == "__main__":
     main()
+
