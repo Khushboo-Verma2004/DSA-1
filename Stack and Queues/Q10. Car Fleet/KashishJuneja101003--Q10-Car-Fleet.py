f424d15034076ed0ec49731cc7db4ac6a9434e9a
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-02-07 07:08:32 +0530

    Update KashishJuneja101003--Q10-Car-Fleet.py

--- Stack and Queues/Q10. Car Fleet/KashishJuneja101003--Q10-Car-Fleet.py
+++ Stack and Queues/Q10. Car Fleet/KashishJuneja101003--Q10-Car-Fleet.py
@@ -1,9 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = prev = 0
-        for pp, ss in sorted(zip(position, speed), reverse=True): 
-            tt = (target - pp)/ss # time to arrive at target 
-            if prev < tt: 
-                ans += 1
-                prev = tt
-        return ans 
+        stack = []
+        for pp, ss in sorted(zip(position, speed), reverse=True):
+            tt = (target - pp) / ss  # Time to reach the target
+            if not stack or tt > stack[-1]:
+                stack.append(tt)
+        return len(stack)
